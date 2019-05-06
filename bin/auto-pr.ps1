76fdadb5db465fe095e48a01f51c755603a30ced
Richard Kuhnt <r15ch13+git@gmail.com>
2019-05-06 18:12:54 +0200

    Incorporate organization and bucket renaming
    See: https://github.com/lukesampson/scoop/issues/3437

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "scoopinstaller/scoop-main:master"
+    [String]$upstream = "ScoopInstaller/Main:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
