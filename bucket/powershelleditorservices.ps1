2510df462741ab38d2a659bffc0344f972b3631e
Olav Rønnestad Birkeland <6450056+o-l-a-v@users.noreply.github.com>
2025-10-22 11:37:15 +0200

    Add PowerShell Editor Services version 4.4.0

---
+++ bucket/powershelleditorservices.ps1
@@ -0,0 +1,12 @@
+{
+    "version": "4.4.0",
+    "homepage": "https://github.com/PowerShell/PowerShellEditorServices",
+    "license": "MIT",
+    "url": "https://github.com/PowerShell/PowerShellEditorServices/releases/download/v4.4.0/PowerShellEditorServices.zip",
+    "hash": "690b91092989a0f66e6f43986166aaef69d64b559a9fda51feed882e1103fbcc",
+    "bin": "PowerShellEditorServices/Start-EditorServices.ps1",
+    "checkver": "github",
+    "autoupdate": {
+        "url": "https://github.com/PowerShell/PowerShellEditorServices/releases/download/v$version/PowerShellEditorServices.zip"
+    }
+}
